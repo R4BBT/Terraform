263a67476f321212e181951ee1ae0a601a173e30
R4BBT <ryanfangk@gmail.com>
2020-12-24 14:22:47 +0000

    12/24/2020

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # VM configuration for Minecraft server
 provider "google" {
-    project = "jenkins-299411"    
+    project = "jenkins-299411" #This is the test project I currently have set up
     region = "us-central1"
     zone = "us-central1-f"
 }
@@ -17,12 +17,12 @@ resource "google_compute_disk" "default" {
 
 resource "google_compute_instance" "vm_instance" {
   name = "mc-server"
-  machine_type = "f1-micro" # f1-micro because it's free
+  machine_type = "f1-micro" #f1-micro because it's free, but it really should be N1-standard-1 or higher
   tags = ["minecraft-server"] 
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1604-xenial-v20201210" #The exact image could be found using the command gcloud compute images list | grep ubuntu
+      image = "ubuntu-os-cloud/ubuntu-1604-xenial-v20201210" #This exact image could be found using the command gcloud compute images list | grep ubuntu
       type = "pd-ssd"
     }
   }
@@ -45,7 +45,7 @@ resource "google_compute_instance" "vm_instance" {
     scopes = ["compute-full", "storage-full"]
   }
 }
-#################################################
+# Configuring firewall rules for Ingress
 resource "google_compute_firewall" "default" {
   name    = "minecraft-rule"
   network = google_compute_network.default.default
@@ -63,4 +63,14 @@ resource "google_compute_firewall" "default" {
 
   source_ranges = ["0.0.0.0/0"]
   target_tags ["minecraft-server"]
+}
+
+resource "google_storage_bucket" "default" {
+  name          = "minecraft-server-backup"
+  location      = "US-central1"
+  project = "jenkins-299411"
+  force_destroy = true
+  storage_class = STANDARD
+  uniform_bucket_level_access = true #Allows IAM permission instead of ACL permission for easier management
+  versioning = true
 }
